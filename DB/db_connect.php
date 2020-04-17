33f77f232eee43c1dfddce2814ca7673cdb97d13
Peter Steele <steelep@mail.nmc.edu>
2020-04-17 15:15:32 -0400

    Initial Commit

--- DB/db_connect.php
+++ DB/db_connect.php
@@ -1,1 +1,26 @@
-<?php$server = "localhost";$username = "";$password = "";$database = "";// Create the connection for the database$connection = new mysqli($server, $username, $password, $database);// Check to see if the connection succeedsif ($connection->connect_error) {    die("Connection has failed: " . $connection ->connect_error);}
+<?php
+
+$server = "localhost";
+
+$username = "";
+
+$password = "";
+
+$database = "";
+
+
+
+// Create the connection for the database
+
+$connection = new mysqli($server, $username, $password, $database);
+
+
+
+// Check to see if the connection succeeds
+
+if ($connection->connect_error) {
+
+    die("Connection has failed: " . $connection ->connect_error);
+
+}
+
